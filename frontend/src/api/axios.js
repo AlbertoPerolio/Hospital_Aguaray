9a058d7c79de849d929f25b2dce0f8469325442c
AlbertoPerolio <albertoperolio0319@gmail.com>
2026-06-08 01:47:11 -0300

    .

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://hospital-aguaray.onrender.com/api",
   timeout: 10000,
   withCredentials: true,
 });
